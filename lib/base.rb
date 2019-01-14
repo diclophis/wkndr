23dd1659e8b4d3f4a751ea897ff38aa43b0c0c4b
Jon Bardin <diclophis@gmail.com>
2019-01-13 18:47:36 -0800

    refactor for 3d base loop

--- lib/base.rb
+++ lib/base.rb
@@ -1,12 +1,16 @@
 #
 
 class Base < Thor
-  def window(name, x, y, fps)
+  def gameloop
     gl = GameLoop.new(self)
-    yield gl
+    #yield gl
+    gl
+  end
 
+  def window(name, x, y, fps)
+    gl = gameloop
     window = Window.new("wkndr", x, y, fps, gl)
-
+    yield window, gl
     window
   end
 
@@ -16,16 +20,22 @@ class Base < Thor
   end
     
   def client
-    window("window", 512, 512, 60) { |gl|
-      log! :in_opened_window, gl
+    #TODO: refactor multi loop
+    window("window", 512, 512, 60) { |window, gl|
+      log! :in_opened_window, window, gl
 
       gl.lookat(0, 0.0, 500.0, 0.0, 0.0, 0.0, 0.01, 200.0)
+      cube = Cube.new(1.0, 1.0, 1.0, 5.0)
 
       gl.play { |global_time, delta_time|
         gl.drawmode {
+          gl.threed {
+            gl.draw_grid(10, 10.0)
+            cube.draw(true)
+          }
+
           gl.twod {
             gl.button(0.0, 0.0, 250.0, 20.0, "start #{global_time}") {
-              #TODO: onclick
               log! :click
             }
           }
