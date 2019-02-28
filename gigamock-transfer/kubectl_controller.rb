338816767c2ef759c3c0899d75786fb14ba52062
Jon Bardin <diclophis@gmail.com>
2019-02-28 09:28:54 +0000

    fix up re-watch

--- gigamock-transfer/kubectl_controller.rb
+++ gigamock-transfer/kubectl_controller.rb
@@ -24,15 +24,6 @@ class Kube
     meta_keys = description["metadata"].keys
 
     case kind
-      when "IngressList"
-        puts [kind].inspect
-
-      when "PodList"
-        puts [kind].inspect
-
-      when "ServiceList"
-        puts [kind].inspect
-
       when "Pod"
         latest_condition = nil
         phase = nil
@@ -97,100 +88,129 @@ class Kube
     parser.on_parse_complete = method(:handle_event_list)
     io = get_yaml_producer_io(kubernetes_resource)
 
-    @watches ||= []
-    @watches << [io, parser]
+    @watches ||= {}
+    @watches[io] = [kubernetes_resource, parser]
+  end
+
+  def rewatch_io(old_io)
+    if ab = @watches[old_io]
+      Process.wait(old_io.pid) rescue Errno::ECHILD
+
+      kubernetes_resource, old_parser = ab
+      puts [:recycling, kubernetes_resource].inspect
+      @changed = true
+
+      @watches.delete(old_io)
+
+      watch!(kubernetes_resource)
+    end
   end
 
   def ingest!
     last_read_bit = nil
-    begin
+    loop do
       begin
-        loop do
-          @watches.each do |io, parser|
-            last_read_bit = io
-            got_read = io.read_nonblock(1)
-            parser << got_read
+        # see whats still running
+        io_to_rewatch = @watches.collect { |io, _|
+          begin
+            Process.kill(0, io.pid)
+            Process.waitpid(io.pid, Process::WNOHANG)
+            nil
+          rescue Errno::ECHILD, Errno::ESRCH
+            io
           end
+        }.compact
+
+        # rewatched closed bits
+        io_to_rewatch.each { |old_io|
+          rewatch_io(old_io)
+        }
+
+        selectable_io = @watches.collect { |io, _| io }.compact
+        a,b,c = IO.select(selectable_io, [], [], 1.0)
+
+        if a
+          a.each { |io|
+            if ab = @watches[io]
+              resource, parser = ab
+              last_read_bit = io
+              got_read = io.read_nonblock(1)
+              parser << got_read
+            end
+          }
         end
-      rescue IO::EAGAINWaitReadable => idle_spin_err
-        selectable_io = @watches.collect { |io, parser| io }.compact
-        a,b,c = IO.select(selectable_io, [], [], 0.001)
-        begin
-          if a.nil? && @changed
-            upstream_map = ""
-            host_to_app_map = "" 
-            app_to_alias_map = ""
 
-            @ingresses && @ingresses.each { |ingress, ing_spec|
-              rules = ing_spec["rules"]
-              rules.each { |rule|
-                host = rule["host"]
-                http = rule["http"]
-                paths = http["paths"]
-                paths.each { |path|
-                  backend = path["backend"]
-                  service_name = backend["serviceName"]
-                  service_port = backend["servicePort"]
+        if a.nil? && @changed
+          upstream_map = ""
+          host_to_app_map = ""
+          app_to_alias_map = ""
 
-                  if @services && found_service = @services[service_name] 
-                    type = found_service["type"]
+          @ingresses && @ingresses.each { |ingress, ing_spec|
+            rules = ing_spec["rules"]
+            rules.each { |rule|
+              host = rule["host"]
+              http = rule["http"]
+              paths = http["paths"]
+              paths.each { |path|
+                backend = path["backend"]
+                service_name = backend["serviceName"]
+                service_port = backend["servicePort"]
 
-                    case type
-                      when "NodePort"
-                        node_port = found_service["ports"].detect { |port|
-                          port["port"] == service_port
-                        }
+                if @services && found_service = @services[service_name]
+                  type = found_service["type"]
 
-                        upstream_map += "upstream #{service_name} {\n  server 127.0.0.1:#{node_port["nodePort"]} fail_timeout=0;\n}\n"
-                        host_to_app_map += "#{host} #{service_name};\n"
-                        app_to_alias_map = "#{service_name} /usr/share/nginx/html/;\n"
+                  case type
+                    when "NodePort"
+                      node_port = found_service["ports"].detect { |port|
+                        port["port"] == service_port
+                      }
 
-                      when "ClusterIP"
-                        cluster_ip = found_service["clusterIP"]
-                        upstream_map += "upstream #{service_name} {\n  server #{cluster_ip}:#{service_port} fail_timeout=0;\n}\n"
-                        host_to_app_map += "#{host} #{service_name};\n"
-                        app_to_alias_map = "#{service_name} /usr/share/nginx/html/;\n"
+                      upstream_map += "upstream #{service_name} {\n  server 127.0.0.1:#{node_port["nodePort"]} fail_timeout=0;\n}\n"
+                      host_to_app_map += "#{host} #{service_name};\n"
+                      app_to_alias_map = "#{service_name} /usr/share/nginx/html/;\n"
 
-                    end
+                    when "ClusterIP"
+                      cluster_ip = found_service["clusterIP"]
+                      upstream_map += "upstream #{service_name} {\n  server #{cluster_ip}:#{service_port} fail_timeout=0;\n}\n"
+                      host_to_app_map += "#{host} #{service_name};\n"
+                      app_to_alias_map = "#{service_name} /usr/share/nginx/html/;\n"
+
                   end
-                }
+                end
               }
             }
+          }
 
-            confd_dir = ENV["MOCK_CONFD_DIR"] || "/etc/nginx/conf.d"
+          confd_dir = ENV["MOCK_CONFD_DIR"] || "/etc/nginx/conf.d"
 
-            File.open("#{confd_dir}/hosts_app.conf", "w+") { |f|
-              f.write(upstream_map)
-            }
+          File.open("#{confd_dir}/hosts_app.conf", "w+") { |f|
+            f.write(upstream_map)
+          }
 
-            File.open("#{confd_dir}/hosts_app.map", "w+") { |f|
-              f.write(host_to_app_map)
-            }
+          File.open("#{confd_dir}/hosts_app.map", "w+") { |f|
+            f.write(host_to_app_map)
+          }
 
-            File.open("#{confd_dir}/hosts_app_alias.map", "w+") {|f|
-              f.write(app_to_alias_map)
-            }
+          File.open("#{confd_dir}/hosts_app_alias.map", "w+") {|f|
+            f.write(app_to_alias_map)
+          }
 
-            unless system("systemctl", "reload", "nginx.service")
-              $stderr.write("bad nginx conf\n")
-              exit(1)
-            end
-
-            $stdout.write("updated vhost table\n")
-            $stdout.flush
-
-            @changed = false
+          unless system("systemctl", "reload", "nginx.service")
+            $stderr.write("bad nginx conf\n")
+            exit(1)
           end
 
-          retry
-        rescue Errno::EPIPE
-          $stderr.write("output closed...")
+          $stdout.write("updated vhost table\n")
+          $stdout.flush
+
+          @changed = false
         end
+
       end
     rescue EOFError => eof_err
-      puts [last_read_bit, eof_err].inspect
-      exit(1)
-      #retry
+      rewatch_io(last_read_bit)
+
+      retry
     end
   rescue Interrupt => ctrlc_err
     exit(0)
@@ -204,5 +224,5 @@ end
 klient = Kube.new
 klient.watch!("pods")
 klient.watch!("services")
-klient.watch!("ingress")
+klient.watch!("ingresses")
 klient.ingest!
