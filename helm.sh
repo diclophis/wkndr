12833de80c98af09ebd90aec7e0ea5933c36c6f0
Jon Bardin <diclophis@gmail.com>
2019-03-17 18:22:33 -0700

    add tls verification for helm

--- helm.sh
+++ helm.sh
@@ -1,6 +1,10 @@
 #
 
+#TODO:
+# Please note: by default, Tiller is deployed with an insecure 'allow unauthenticated users' policy.
+# To prevent this, run `helm init` with the --tiller-tls-verify flag.
+
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'      
-helm init --service-account tiller --upgrade
+helm init --tiller-tls-verify --service-account tiller --upgrade
