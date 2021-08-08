af9f21ed87c9c9a1d30073a4083efc51db87afd9
Jon Bardin <diclophis@gmail.com>
2021-08-08 19:57:37 -0400

    bits

--- attic/run-debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# dd if=/dev/urandom of=/var/tmp/big.data bs=1M count=1; rm debug; wget --debug http://localhost:8081/debug; shasum debug big.data
-
-#docker run --rm -p 8000:8000 wkndr:latest /bin/busybox tar -czf - public | tar zxvf -
-
-docker run --rm -p 8000:8000 wkndr:latest 
