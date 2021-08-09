16b0b628def6f6ae34f103d1295d7b2594abf97e
Jon Bardin <diclophis@gmail.com>
2021-08-08 23:25:03 -0400

    more bits

--- config/emscripten.rb
+++ config/emscripten.rb
@@ -1,21 +1,38 @@
 #
 
-#EMSDK_VERSION="1.38.36"
-
 require_relative './mruby.rb'
 
-
 MRuby::CrossBuild.new('emscripten') do |conf|
+  #EMSDK_VERSION="1.38.36"
   # load specific toolchain settings
   #toolchain :clang
-
   #enable_debug
   #conf.disable_cxx_exception
-
+  #conf.enable_cxx_exception
+  #conf.disable_cxx_exception
   #enable_debug
+  #conf.gem :core => "mruby-bin-mirb"
+  #conf.gem :core => "mruby-time"
+  #  ff.cc.defines = %w{MSGPACK_ENDIAN_LITTLE_BYTE=1 TEST_BIG_ENDIAN=0}
+  #end
+  #conf.gem :core => "mruby-io"
+  ##conf.gem :github => "h2so5/mruby-pure-regexp"
+  #conf.cc.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emcc"
+  #conf.linker.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emcc"
+  #conf.archiver.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emar"
+  #conf.cc.command = "/root/emsdk/fastcomp/emscripten/emcc"
+  #conf.linker.command = "/root/emsdk/fastcomp/emscripten/emcc"
+  #conf.archiver.command = "/root/emsdk/fastcomp/emscripten/emar"
+  #conf.cc.defines = %w{MSGPACK_ENDIAN_LITTLE_BYTE=1 TEST_BIG_ENDIAN=0}
+  #do |ff|
+  #  #ff.cc.command = "/root/emsdk/upstream/emscripten/emcc"
+  #end
+  #  #cc.defines << "MRB_USE_CXX_EXCEPTION=1"
+  #, "-DMSGPACK_ENDIAN_LITTLE_BYTE=1", "-DTEST_BIG_ENDIAN=0"]
+
+
   conf.bins = []
 
-  #conf.gem :core => "mruby-bin-mirb"
   conf.gem :core => "mruby-math"
   conf.gem :core => "mruby-random"
   conf.gem :core => "mruby-enum-ext"
@@ -25,37 +42,15 @@ MRuby::CrossBuild.new('emscripten') do |conf|
   conf.gem :core => "mruby-string-ext"
   conf.gem :core => "mruby-eval"
   conf.gem :core => "mruby-pack"
-  #conf.gem :core => "mruby-time"
 
   conf.gem :github => "yui-knk/mruby-set"
   conf.gem :github => "Asmod4n/mruby-simplemsgpack"
-  #do |ff|
-  #  ff.cc.defines = %w{MSGPACK_ENDIAN_LITTLE_BYTE=1 TEST_BIG_ENDIAN=0}
-  #end
-  
-  #conf.gem :core => "mruby-io"
-
-  ##conf.gem :github => "h2so5/mruby-pure-regexp"
-
-  #conf.cc.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emcc"
-  #conf.linker.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emcc"
-  #conf.archiver.command = "/root/emsdk/emscripten/#{EMSDK_VERSION}/emar"
-
-  #conf.cc.command = "/root/emsdk/fastcomp/emscripten/emcc"
-  #conf.linker.command = "/root/emsdk/fastcomp/emscripten/emcc"
-  #conf.archiver.command = "/root/emsdk/fastcomp/emscripten/emar"
-
-  #conf.cc.defines = %w{MSGPACK_ENDIAN_LITTLE_BYTE=1 TEST_BIG_ENDIAN=0}
 
   conf.cc.command = "/root/emsdk/upstream/emscripten/emcc"
   conf.linker.command = "/root/emsdk/upstream/emscripten/emcc"
   conf.archiver.command = "/root/emsdk/upstream/emscripten/emar"
 
-  #conf.enable_cxx_exception
-  #conf.disable_cxx_exception
-
   conf.cc do |cc|
-    #cc.defines << "MRB_USE_CXX_EXCEPTION=1"
-    cc.flags = ["-s DISABLE_EXCEPTION_CATCHING=0", "-s USE_ZLIB=1", "-O3"] #, "-DMSGPACK_ENDIAN_LITTLE_BYTE=1", "-DTEST_BIG_ENDIAN=0"]
+    cc.flags = ["-O3"]
   end
 end
--- gigamock-transfer/run-debug.sh
+++ gigamock-transfer/run-debug.sh
@@ -6,6 +6,6 @@ set -x
 # dd if=/dev/urandom of=/var/tmp/big.data bs=1M count=1; rm debug; wget --debug http://localhost:8081/debug; shasum debug big.data
 polly build Dockerfile
 
-docker run --rm -p 8000:8000 wkndr:latest /bin/busybox tar -czf - public | tar zxvf -
+docker run --rm wkndr:latest /bin/busybox tar -czf - public | tar zxvf -
 
 #docker run --rm -p 8000:8000 wkndr:latest 
